b9cacc457789e19829e4a7fa6249ca186ff4ce8d
Martin Lindström <martinllindstroem@gmail.com>
2020-12-08 17:41:44 +0100

    fixed bug

--- config/di/ip.php
+++ config/di/ip.php
@@ -14,15 +14,23 @@ return [
                 // Load the configuration files
                 $cfg = $this->get("configuration");
 
-                if (file_exists("config/private_keys.php")) {
-                    $config = $cfg->load("private_keys");
+                // if (file_exists("config/private_keys.php")) {
+                //     $config = $cfg->load("private_keys");
 
-                    // Set the api key configuration
+                //     // Set the api key configuration
+                //     $key = $config["config"]["ipStack"] ?? "";
+                //     $ip->setApiKey($key);
+                // } else {
+                //     $key = getenv("IPSTACK_KEY") ?? "";
+                //     $ip->setApiKey($key);
+                // }
+                if (!getenv("IPSTACK_KEY")) {
+                    $config = $cfg->load("private_keys");
                     $key = $config["config"]["ipStack"] ?? "";
                     $ip->setApiKey($key);
                 } else {
-                    $key = getenv("IPSTACK_KEY") ?? "";
-                    $ip->setApiKey($key);
+                        $key = getenv("IPSTACK_KEY") ?? "";
+                        $ip->setApiKey($key);
                 }
                 return $ip;
             }
